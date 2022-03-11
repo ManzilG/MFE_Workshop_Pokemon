fa6662a6d4e1c38d155b15ade8b82a03567c4e54
Manzil Gajurel <manzil.gajurel@hertz.com>
2022-03-10 21:32:43 -0600

    Added local storage for current input and saved Images.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ window.onload = function () {
     let frontImage = "";
     let backImage = "";
     let currentImage = "";
+    const savedData = localStorage.getItem("data") ? JSON.parse(localStorage.getItem("data")) : null;
+    const savedInput = localStorage.getItem("input") ? localStorage.getItem("input") : null;
+    const savedItems = localStorage.getItem("savedItems") ? JSON.parse(localStorage.getItem("savedItems")) : null;
 
     searchText.addEventListener("keydown", function (e) {
         if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
@@ -18,22 +21,14 @@ window.onload = function () {
         fetch('https://pokeapi.co/api/v2/pokemon/' + input)
             .then(response => response.json())
             .then(data => {
+                localStorage.setItem("data", JSON.stringify(data));
+                localStorage.setItem("input", input);
                 showPokemonImage(data);
                 populatePokemonInfo(data);
                 saveButton.onclick = () => {
                     storePokemon(data);
                 }
             })
-    }
-
-    pokemon.onclick = () => {
-        if (currentImage === frontImage) {
-            currentImage = backImage;
-            pokemon.src = backImage;
-        } else {
-            currentImage = frontImage;
-            pokemon.src = currentImage;
-        }
     }
 
     const populatePokemonInfo = (data) => {
@@ -50,15 +45,45 @@ window.onload = function () {
 
     }
 
-    const storePokemon = (data) => {
+    const createImage = (data) => {
         let img = document.createElement('img');
         img.src = data.sprites.front_default;
         document.getElementById('pokemon-storage').appendChild(img);
-
         img.onclick = () => {
             showPokemonImage(data);
             populatePokemonInfo(data);
         }
+    }
+
+    const storePokemon = (data) => {
+        createImage(data);
+        let savedImages = JSON.parse(localStorage.getItem("savedItems"));
+        savedImages.push(data);
+        localStorage.setItem("savedItems", JSON.stringify(savedImages));
+    }
+
+    pokemon.onclick = () => {
+        if (currentImage === frontImage) {
+            currentImage = backImage;
+            pokemon.src = backImage;
+        } else {
+            currentImage = frontImage;
+            pokemon.src = currentImage;
+        }
+    }
+
+    if(savedData && savedInput) {
+        searchText.value = savedInput;
+        showPokemonImage(savedData);
+        populatePokemonInfo(savedData);
+        saveButton.onclick = () => storePokemon(savedData);
+
+    }
+    if(savedItems) {
+        savedItems.forEach(savedItem => {
+            createImage(savedItem);
+        })
+
     }
 
 }
